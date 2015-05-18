eac9494f863191c05eda8f08f87e49bd57c8e14a
QuirijnLukasVermeire <quirijn.vermeire@hotmail.com>
2015-05-18 21:44:11 +0100

    Working Picture code
    When Pin 27 goes High, a picture is taken with the camera. The basis
    behind this is that the scanner clamp will be a switch and allow pin 27
    to go high and take a picture (with a possible delay) when the page is
    "clamped on". Will only take a picture once per high pulse, and waits
    for it to go low, then high again (Schmitt trigger).

--- CamPicTest.py
+++ CamPicTest.py
@@ -1,7 +1,7 @@
 ################################################################
 #           Cam test when Pin 17 goes high Test 1              #
-#          Created 17/05/2015 by Quirijn Lukas Vermeire        #
-#            Circuit setup: DC 5v -> Resistor -> Pin 17        #
+#         Developed 18/05/2015 by Quirijn Lukas Vermeire       #
+#       Co-developed by: Anhad Sandhu, Abdullah El Agha        #
 #         Pin layout image: http://i.imgur.com/vabFhTG.png     #
 ################################################################
 
@@ -11,20 +11,17 @@ import time
 import RPi.GPIO as GPIO
 #GPIO setup
 GPIO.setmode(GPIO.BCM)      #Setup Pin IO
-GPIO.setup(17,GPIO.IN)      #Setup pin 17 as Input
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #Name setup
 camera = picamera.PiCamera()#Giving the camera object a name
 prev_input = 0              #Variable to check previous pininput
-Number = 1                  #Variable for naming the file
-
-while True:
-  input = GPIO.input(17)    #take new reading
-  if ((not prev_input) and input):           #If PIN17 went LO->HI
-    camera.capture('image' + Number +'.jpg') #take a picture
-    Number += 1             #increase filename # by 1
-  prev_input = input        #update previous input
-  time.sleep(0.12)          #debounce pause 
-
-
-
-
+Number = 1                  #Variable for naming the fil
+GPIO.add_event_detect(27, GPIO.RISING) #Detect if ping goes to high
+#take a picture function
+def my_callback(Unused):
+  global Number
+  camera.capture('image' + str(Number) +'.jpg') #take a picture
+  print('Took picture #' + str(Number))		#print to console
+  Number += 1             #increase filename # by 1
+#check if button is pushed function
+GPIO.add_event_callback(27, my_callback) #calls my_callback
